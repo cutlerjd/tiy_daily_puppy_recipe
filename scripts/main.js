ee6fcc29d2379af723d135b223d24f6165e147fb
Jase Cutler <jasecutler@gmail.com>
2017-07-18 12:13:40 -0700

    worky work

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,32 @@
-console.log("Hello")
+let recipesContainer = document.getElementsByClassName('.recipesContainer')[0]
+let recipes = undefined
+let url = new URL("/api/?q=garlic", "http://www.recipepuppy.com")
+
+fetch(url)
+    .then(function (response) {
+        console.log(response)
+        return response.json()
+        if (
+            response.headers.get("content-type").indexOf("text/javascript") !== -1
+        ) {
+            //Converts it to JSON
+            return response.json;
+        } else {
+            throw new TypeError(
+                'Response from "' + url + '" has unexpected "content-type"'
+            );
+        }
+    })
+    .then(function (data) {
+        console.log('JSON from "' + url + '" parsed successfully!');
+        console.log(data);
+        recipes = data
+    })
+    .catch(function (error) {
+        console.error(error.message);
+    });
+
+function fillRecipes() {
+    let htmlString = ''
+
+}
